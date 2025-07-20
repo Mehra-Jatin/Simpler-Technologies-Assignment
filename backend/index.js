2ffb73d94d141c1b4a08bffdc523df2f1d1eac1b
Mehra-Jatin <jatinmehra578@gmail.com>
2025-07-21 02:26:00 +0530

    initial commit

---
+++ backend/index.js
@@ -0,0 +1,21 @@
+import express from "express";
+import dotenv from "dotenv";
+import {connectDB} from "./db/connectDB.js";
+
+dotenv.config();
+
+const app = express();
+app.use(express.json());
+const PORT = process.env.PORT || 5000;
+
+app.get("/", (req, res) => {
+  res.send("API is running...");
+});
+
+app.listen(PORT, () => {
+    connectDB();
+  console.log(`Server is running on port ${PORT}`);
+});
+
+
+
